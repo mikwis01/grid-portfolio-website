e57e143c0bfb3eaf96662869b1c14ada2446dd5e
Mikołaj Wiśniewski <mikolajwisniewski01@gmail.com>
2022-11-08 19:07:10 +0100

    Meta tags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,11 @@ export default function RootLayout({
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"
 				/>
+				<meta
+					name="description"
+					content="Hi, im Mikołaj - front end developer based in Bydgoszcz, Poland."
+				/>
+				<title>Mikołaj Wiśniewski | Front-end developer</title>
 			</head>
 			<body>{children}</body>
 		</html>
